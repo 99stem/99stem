23bafaee762750ccc7876f8733a6d7381f5a9b5b
99stem <smartstn@gmail.com>
2016-01-26 18:13:18 +0100

    Update a.java

--- a.java
+++ a.java
@@ -1,7 +1,26 @@
+/*
+* Made by me 99stem for personal reasons
+*/
+
+import java.util.Scanner;
+
 public class a
 {
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
+    
+    Scanner ifStatement = new Scanner(System.in);
+    System.out.println("Enter value for X: ");
+    int x = ifStatement.next();
+    
+    if(x > 0)
+    {
+      System.out.println("The value you entered is higher than 0");
+    }
+    else
+    {
+      System.out.println("The value you entered is NOT higher than 0");
+    }
   }
 }
